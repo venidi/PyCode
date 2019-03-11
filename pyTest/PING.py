92c56c9fd0e709c2b75394d8bd63d8ec288c7476
Veniendeavor <venidi@163.com>
2019-03-11 21:35:52 +0800

    PING

---
+++ pyTest/PING.py
@@ -0,0 +1,22 @@
+# encoding: utf-
+
+import time
+import struct
+import socket
+import select
+import sys
+
+
+def checksum(data):
+    n = len(data)
+    m = n % 2
+    sum = 0
+    for i in range(0, n-m, 2):
+        sum += (data[i]) + ((data[i+1]) << 8)
+    if m:
+        sum += (data[-1])
+    sum = (sum >> 16) + (sum & 0xffff)
+    sum += (sum >> 16)
+    answer = ~sum & 0xffff
+    answer = answer >> 8 | (answer << 8 & 0xff00)
+    return answer
